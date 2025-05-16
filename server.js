6c797513dd65270383da8818c3f2bd20a9e7503c
sriharsha <sriharshadabbiru04@gmail.com>
2025-05-16 16:43:29 +0530

    packages setup(4)

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
 
+//dot config
+dotenv.config();
+
+//express rest object
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to Blood Bank",
-  });
-});
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
-const PORT = 8080;
+//sample route
+app.use("/api/v1/test", require("./routes/testRoutes"));
 
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
 app.listen(PORT, () => {
-  console.log("Server is running");
+  console.log(
+    `Node Server is running In ${process.env.DEV_MODE} Mode on Port ${process.env.PORT}`
+      .bgBlue.white
+  );
 });
